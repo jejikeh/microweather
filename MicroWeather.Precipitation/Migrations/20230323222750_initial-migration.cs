3331fe547d87476f1b7eee2627047a7268e42d72
jejikeh <jejikeh@gmail.com>
2023-03-24 19:19:18 +0300

    psql migrations

---
+++ MicroWeather.Precipitation/Migrations/20230323222750_initial-migration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroWeather.Precipitation.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Precipitations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AmountInches = table.Column<decimal>(type: "numeric", nullable: false),
+                    WeatherType = table.Column<string>(type: "text", nullable: false),
+                    ZipCode = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Precipitations", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Precipitations_Id",
+                table: "Precipitations",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Precipitations");
+        }
+    }
+}
